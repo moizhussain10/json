cf1984075292e8b6678d02cf5063874175fbd189
unknown <admin@lab-201-pc-02.labdc.com>
2026-07-28 10:34:45 +0500

    ok

---
+++ footer.js
@@ -0,0 +1,48 @@
+let footer = `<footer class="bg-dark text-white pt-5 pb-4">
+  <div class="container text-center text-md-start">
+    <div class="row text-center text-md-start">
+
+      <!-- Column 1: Brand/About -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Company Name</h5>
+        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+
+      <!-- Column 2: Products -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Products</h5>
+        <p><a href="#" class="text-white text-decoration-none">The Providers</a></p>
+        <p><a href="#" class="text-white text-decoration-none">Creativity</a></p>
+        <p><a href="#" class="text-white text-decoration-none">Source Files</a></p>
+      </div>
+
+      <!-- Column 3: Useful Links -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Useful links</h5>
+        <p><a href="#" class="text-white text-decoration-none">Your Account</a></p>
+        <p><a href="#" class="text-white text-decoration-none">Become an Affiliate</a></p>
+        <p><a href="#" class="text-white text-decoration-none">Shipping Rates</a></p>
+      </div>
+
+      <!-- Column 4: Contact info -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact</h5>
+        <p>New York, NY 10012, US</p>
+        <p>info@gmail.com</p>
+        <p>+ 01 234 567 88</p>
+      </div>
+      
+    </div>
+
+    <hr class="mb-4">
+
+    <!-- Bottom Bar -->
+    <div class="row align-items-center">
+      <div class="col-md-7 col-lg-8">
+        <p>© 2026 Copyright: <a href="#" class="text-warning text-decoration-none"><strong>MDBootstrap.com</strong></a></p>
+      </div>
+    </div>
+  </div>
+</footer>`
+
+export default footer
--- main.js
+++ main.js
@@ -1,24 +1,43 @@
-fetch('https://dummyjson.com/products')
-    .then((res) => res.json())
-    .then((data) => {
-        let user = ""
+// fetch('https://dummyjson.com/products')
+//     .then((res) => res.json())
+//     .then((data) => {
+//         let user = ""
 
-        data.products.forEach((u) => {
-            user += `
+//         data.products.forEach((u) => {
+//             user += `
             
-                <div class="card">
-                    <div class="img">
-                        <img src="${u.images[0]}" alt="">
-                    </div>
-                <div class="content">
-                    <h3>${u.id}</h3>
-                    <h3>${u.title}</h3>
-                    <h5>price:- ${u.price}</h5>
-                </div>
-                </div>
-            `
-        });
-        document.getElementById("data").innerHTML = user
-    })
+//                 <div class="card">
+//                     <div class="img">
+//                         <img src="${u.images[0]}" alt="">
+//                     </div>
+//                 <div class="content">
+//                     <h3>${u.id}</h3>
+//                     <h3>${u.title}</h3>
+//                     <h5>price:- ${u.price}</h5>
+//                 </div>
+//                 </div>
+//             `
+//         });
+//         document.getElementById("data").innerHTML = user
+//     })
+
+// let data={
+//     id:1,
+//     name:"moiz hussain",
+//     age: 19
+// }
 
 
+// console.log(data["id"])
+
+// for(key in data){
+//     console.log(data[key])
+// }
+
+
+import header from "./header.js";
+import footer from "./footer.js";
+
+
+document.getElementById("header").innerHTML = header
+document.getElementById("footer").innerHTML = footer
